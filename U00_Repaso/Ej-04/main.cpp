d0638849d79590f9c5cc26afba363eef433c3eff
agus98 <agustinvidela0198@gmail.com>
2019-08-13 16:50:56 -0300

    Completado ejercicio 04

--- U00_Repaso/Ej-04/main.cpp
+++ U00_Repaso/Ej-04/main.cpp
@@ -1,8 +1,16 @@
 #include <iostream>
 
-using namespace std;
-
 int main() {
-    cout << "Ejercicio 00/04\n" << endl;
+    std::cout << "Ejercicio 00/04\n" << std::endl;
+    int a, hora=0, min=0, seg=0;
+    std::cout << "Ingrese tiempo en segundos: ";
+    std::cin >> a;
+    seg = a % 60;
+    min = a / 60;
+    if(min%60 != 0){
+        hora = min/60;
+        min %= 60;
+    }
+    std::cout << "Hora corregida: " << hora << ":" << min << ":" << seg;
     return 0;
 }
