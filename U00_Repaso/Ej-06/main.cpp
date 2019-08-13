06e865d1ff2e6dfe4595f897acba037ad41111a3
agus98 <agustinvidela0198@gmail.com>
2019-08-13 17:06:08 -0300

    Completado ejercicio 06

--- U00_Repaso/Ej-06/main.cpp
+++ U00_Repaso/Ej-06/main.cpp
@@ -1,17 +1,27 @@
 #include <iostream>
 #include <ctime>
 
-using namespace std;
-
 int main() {
-    clock_t begin;
-    begin = clock();
+    int array[1024];
 
-    cout << "Comenzando a medir Tiempo\n" << endl;
-    clock_t end = clock();
+    double begin = clock();
+
+    std::cout << "Comenzando a medir Tiempo\n" << std::endl;
+
+    for(int i=0; i<=1023; i++){
+        array[i] = i;
+    }
+
+
+    double end = clock();
 
     double elapsed_secs = double(end - begin) / CLOCKS_PER_SEC;
 
-    cout << "Tardo elapsed_secs" << elapsed_secs << "\n" << std::endl;
+    std::cout << "Tardo elapsed_secs" << elapsed_secs << "\n" << std::endl;
+
+    //creo que está bien pero devuleve 0 porque es muy rapido xd
+    for(int i=0; i<=1023; i++){
+        std::cout << array[i] << std::endl;
+    }
     return 0;
 }
