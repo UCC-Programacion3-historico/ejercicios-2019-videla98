9da3a7104eb8751156f7edd47758f56237ef5787
agus98 <agustinvidela0198@gmail.com>
2019-08-13 16:27:27 -0300

    Terminado ejercicio 03

--- U00_Repaso/Ej-03/Calculadora.cpp
+++
@@ -1,1 +0,0 @@
-#include "Calculadora.h"
--- U00_Repaso/Ej-03/main.cpp
+++ U00_Repaso/Ej-03/main.cpp
@@ -1,9 +1,21 @@
 #include <iostream>
 #include "Calculadora.h"
 
-using namespace std;
-
 int main() {
-    cout << "Ejercicio 00/03\n" << endl;
+    Calculadora <int, float>miCalc;
+    int a, b;
+    std::cout << "Ejercicio 00/03\n" << std::endl;
+    std::cout << "\nDividendo: ";
+    std::cin >> a;
+    std::cout << "\nDivisor: ";
+    std::cin >> b;
+    try{
+        if(b==0)
+            throw 1;
+        std::cout << "\nResultado: " << miCalc.divi(a, b) << std::endl;
+    }
+    catch (int x){
+        std::cout << "\nLa division por 0 no esta definida. ERROR: " << x;
+    }
     return 0;
 }
