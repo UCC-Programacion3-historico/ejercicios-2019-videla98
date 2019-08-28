c0f972995826d274721fc6f15887f7356df20407
agus98 <agustinvidela0198@gmail.com>
2019-08-28 19:33:32 -0300

    Completo el ej U02_01

--- U02_Listas/Ej-01/fnInvierte.h
+++ U02_Listas/Ej-01/fnInvierte.h
@@ -9,8 +9,11 @@ void fnInvierte (Lista<T> *lis);
 
 template <class T>
 void fnInvierte (Lista<T> *lis){
-
+    int t = lis->getTamanio();
+    for(int i = 0; i<t; i++) {
+        lis->insertar(i, lis->getDato(t - 1));
+        lis->remover(t);
+    }
 }
-
 
 #endif //FNINVIERTE_H
--- U02_Listas/Ej-01/main.cpp
+++ U02_Listas/Ej-01/main.cpp
@@ -1,8 +1,26 @@
 #include <iostream>
 #include "../Lista/Lista.h"
 #include "fnInvierte.h"
+#include <conio.h>
 
 int main() {
     std::cout << "Ejercicio 02/01\n" << std::endl;
+    Lista<int> lista;
+    int dato;
+    std::cout << "Insertar elementos de la lista" << std::endl;
+    for(int i=0; i<8; i++){
+        std::cin >> dato;
+        lista.insertarUltimo(dato);
+    }
+
+    fnInvierte(&lista);
+
+    std::cout << "\nLista Invertida: " << std::endl;
+
+    for(int i=0; i<8; i++){
+        std::cout << lista.getDato(i) << std::endl;
+    }
+
+    getch();
     return 0;
 }
