a7639bac9514248f3aa734b3a7fe9530944a3fd9
agus98 <agustinvidela0198@gmail.com>
2019-08-28 20:07:28 -0300

    Completo el ej U02_03

--- U02_Listas/Ej-03/main.cpp
+++ U02_Listas/Ej-03/main.cpp
@@ -3,5 +3,35 @@
 
 int main() {
     std::cout << "Ejercicio 01/03\n" << std::endl;
+    Lista<float> lista;
+    float dato;
+    int x;
+
+    std::cout << "Insertar elementos de la lista" << std::endl;
+    for(int i=0; i<8; i++) {
+        std::cin >> dato;
+        lista.insertarUltimo(dato);
+    }
+
+    std::cout << "\nCantidad de elementos en la lista: " << lista.getTamanio();
+    std::cout << "\nIngrese posicion del elemento a eliminar";
+    std::cin >> x;
+    try{
+        lista.remover(x);
+    }catch (int exception){
+        switch(exception){
+            case 404:
+                std::cout << "404 posicion no excontrada" << std::endl;
+                break;
+            default:
+                std::cout << "vs decis";
+        }
+    }
+
+    for(int i=0; i<lista.getTamanio(); i++){
+        std::cout << lista.getDato(i) << std::endl;
+    }
+
+    system("Pause");
     return 0;
 }
