3e563823032970c028181ddc0be26f545d840fd2
agus98 <agustinvidela0198@gmail.com>
2019-09-25 10:02:31 -0300

    corregidas advertencias

--- U04_Colas/Ej-01/main.cpp
+++ U04_Colas/Ej-01/main.cpp
@@ -4,13 +4,13 @@
 int main() {
     std::cout << "Ejercicio 04/01\n" << std::endl;
     Cola<char> c1, c2;
-    unsigned int n, m;
+    unsigned int n, m, i;
     char temp1, temp2;
 
     std::cout<<"Ingrese el tamaño de la cola 1"<<std::endl;
     std::cin>>n;
     std::cout<<"Ingrese datos..."<<std::endl;
-    for(int i=0;i<n;i++) {
+    for(i=0;i<n;i++) {
         std::cin>>temp1;
         c1.encolar(temp1);
     }
@@ -18,7 +18,7 @@ int main() {
     std::cout<<"Ingrese el tamaño de la cola 2"<<std::endl;
     std::cin>>m;
     std::cout<<"Ingrese datos..."<<std::endl;
-    for(int i=0;i<m;i++) {
+    for(i=0;i<m;i++) {
         std::cin>>temp2;
         c2.encolar(temp2);
     }
@@ -26,7 +26,7 @@ int main() {
     if(n != m){
         std::cout<<"Las colas no son iguales";
     } else {
-        for (int i=0;i<n;i++) {
+        for (i=0;i<n;i++) {
             temp1 = c1.desencolar();
             temp2 = c2.desencolar();
             if (temp1 != temp2) {
