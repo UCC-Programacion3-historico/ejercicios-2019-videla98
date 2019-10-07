c3ff384426e9cfb77a780bd173e60d5eeb35f3e0
agus98 <agustinvidela0198@gmail.com>
2019-10-07 18:14:57 -0300

    Completado Ej 04 de recursividad

--- U01_Recursividad/Ej-04/main.cpp
+++ U01_Recursividad/Ej-04/main.cpp
@@ -3,5 +3,21 @@
 
 int main() {
     std::cout << "Ejercicio 01/04\n" << std::endl;
+    int a = 0,
+        b = 0,
+        aux = 0;
+
+    std::cout << "Ingrese a y b" << std::endl;
+
+    std::cin >> a >> b;
+
+    if(b > a) {
+        aux = b;
+        b = a;
+        a = aux;
+    }
+
+    std::cout << "Maximo comun divisor: " << mcd(a, b);
+
     return 0;
 }
